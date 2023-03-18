db995d1beb8209c6b271f5d7b188b3e290ac2d22
rpONE <rpone@rpone.top>
2023-03-18 12:57:20 +0800

    写编辑按键布局文件

--- app/src/main/java/ItemAdapter.kt
+++ app/src/main/java/ItemAdapter.kt
@@ -1,12 +1,17 @@
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.rpone.floatingbuttons.R
 import com.rpone.floatingbuttons.model.ButtonInfo
+import java.io.File
 
 class ItemAdapter(
     private val context: Context,
